cc60a717a61dfb206cd615e47b72c18dd8353fe6
Maria Walker <mariawalker3232@gmail.com>
2022-04-02 18:13:25 -0600

    Saturday

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let cash = 100
+let points = 0
 
 let clickUpgrades = {
   peanutbutter: {
@@ -15,18 +16,17 @@ let clickUpgrades = {
 
 let autoUpgrades = {
   walk: {
-    price: 50,
+    price: 100,
     quantity: 0,
     multiplier: 50
   },
   zoomies: {
-    price: 100,
+    price: 50,
     quantity: 0,
     multiplier: 100
   }
 }
 
-let points = 0
 
 function mine() {
   points++
@@ -34,23 +34,46 @@ function mine() {
   multiplier()
 }
 
-function updatePoints() {
-  document.getElementById('goodgirlpoints').innerText = points
-  // console.log(points)
+function drawcost() {
+  let chickenPrice = clickUpgrades.chicken.price
+  document.getElementById('chickenCost').innerText = '$' + chickenPrice
+  let PBPrice = clickUpgrades.peanutbutter.price
+  document.getElementById('PBCost').innerText = '$' + PBPrice
+  let walkPrice = autoUpgrades.walk.price
+  document.getElementById('walkCost').innerText = '$' + walkPrice
+  let zoomPrice = autoUpgrades.zoomies.price
+  document.getElementById('zoomCost').innerText = '$' + zoomPrice
 }
+
+//#region Click Upgrades
 
 function buyPeanutButter() {
   let price = clickUpgrades.peanutbutter.price
   if (cash >= price) {
     cash -= price
     let newquantity = clickUpgrades.peanutbutter.quantity += 1
+    clickUpgrades.peanutbutter.price += 5
+    insertClicker()
     // console.log('buy', cash, newquantity)
   }
   else {
-    console.log('no money fo peanot buttor')
+    alert('no mor money')
   }
   updateMoney()
   updateQuantity()
+}
+
+function insertClickerPeanut() {
+  let peanuts = clickUpgrades.peanutbutter.quantity
+  if (peanuts >= 1) {
+    document.getElementById('clickIconPeanut').classList.add('mdi', 'mdi-peanut')
+  }
+}
+function insertClickerChicken() {
+  let chicken = clickUpgrades.chicken.quantity
+  if (chicken >= 1) {
+    document.getElementById('clickIconChicken').classList.add('mdi', 'mdi-food-drumstick')
+  }
 }
 
 function buyChicken() {
@@ -58,27 +81,44 @@ function buyChicken() {
   if (cash >= price) {
     cash -= price
     let newquantity = clickUpgrades.chicken.quantity += 1
+    clickUpgrades.chicken.price += 10
     // console.log('buy', cash, newquantity)
   }
   else {
-    console.log('no money fo chicken')
+    alert('no mor money')
   }
   updateMoney()
   updateQuantity()
+  insertClickerChicken()
 }
+
+//#endregion
+
+//#region Auto Upgrades
 
 function buyWalk() {
   let price = autoUpgrades.walk.price
   if (cash >= price) {
     cash -= price
     let newquantity = autoUpgrades.walk.quantity += 1
+    autoUpgrades.walk.price += 50
+    walk()
     // console.log('buy', cash, newquantity)
   }
   else {
-    console.log('no money fo walk')
+    alert('no mor money')
   }
   updateMoney()
   updateQuantity()
+}
+
+function walk() {
+  // on each zoomies purchase myra runs faster for 5 seconds
+  let quantity = autoUpgrades.zoomies.quantity
+  let num1 = quantity -= 10
+  if (num1 <= 1) { num1 += 5 }
+  let zoom = document.getElementById('marquee')
+  let zoomAmount = zoom.setAttribute('scrollamount', num1, num1)
 }
 
 function buyZoomies() {
@@ -86,14 +126,35 @@ function buyZoomies() {
   if (cash >= price) {
     cash -= price
     let newquantity = autoUpgrades.zoomies.quantity += 1
+    autoUpgrades.zoomies.price += 100
     // console.log('buy', cash, newquantity)
+    zoomies()
   }
   else {
-    console.log('no money fo zoomies')
+    alert('no mor money')
   }
   updateMoney()
   updateQuantity()
 }
+
+function zoomies() {
+  // on each zoomies purchase myra runs faster for 5 seconds
+  let quantity = autoUpgrades.zoomies.quantity
+  let num1 = quantity += 10
+  let zoom = document.getElementById('marquee')
+  let zoomAmount = zoom.setAttribute('scrollamount', num1, num1)
+}
+
+function collectAutoUpgrades() {
+  let walkpoints = autoUpgrades.walk.quantity * autoUpgrades.walk.multiplier
+  cash += walkpoints
+  let zoompoints = autoUpgrades.zoomies.quantity * autoUpgrades.zoomies.multiplier
+  cash += zoompoints
+  updateMoney()
+}
+//#endregion
+
+//#region Updates
 
 function updateMoney() {
   document.getElementById('cash').innerText = cash
@@ -108,20 +169,23 @@ function updateQuantity() {
   document.getElementById('walkQuantity').innerText = walkquantity
   let zoomquantity = autoUpgrades.zoomies.quantity
   document.getElementById('zoomiesQuantity').innerText = zoomquantity
+  drawcost()
+}
+
+function updatePoints() {
+  document.getElementById('goodgirlpoints').innerText = points
+  // console.log(points)
 }
 
 function multiplier() {
-  let multiplier = clickUpgrades.peanutbutter.quantity
+  let pbmultiplier = clickUpgrades.peanutbutter.quantity * clickUpgrades.peanutbutter.multiplier
+  let chickenmultiplier = clickUpgrades.chicken.quantity * clickUpgrades.chicken.multiplier
+  let multiplier = pbmultiplier + chickenmultiplier
   points += multiplier
   updatePoints()
 }
 
-// testing auto upgrades does it log 50 if I purchase 1 walk
-function collectAutoUpgrades() {
-  let walkpoints = autoUpgrades.walk.quantity * autoUpgrades.walk.multiplier
-  points += walkpoints
-  updatePoints()
-}
-
+//#endregion
 
 setInterval(collectAutoUpgrades, 3000);
+drawcost()
